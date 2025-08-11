a98449fce7fe5657bd6e1763e6e94cc3d8be9ae9
markglasman <glasman.mark@gmail.com>
2025-08-11 16:20:26 -0400

    init commit

---
+++ src/client/App.jsx
@@ -0,0 +1,88 @@
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [pokemon, setPokemon] = useState(null);
+  const [pokemen, setPokemen] = useState([]);
+  const [pokeImages, setPokeImages] = useState([]);
+
+  useEffect(() => {
+    const getPokemon = async () => {
+      try {
+        const response = await axios.get(
+          "https://pokeapi.co/api/v2/pokemon/ditto"
+        );
+        setPokemon(response);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getPokemon();
+  }, []);
+
+  useEffect(() => {
+    const getPokemen = async () => {
+      try {
+        const pokemen = await axios.get(
+          "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
+        );
+        setPokemen(pokemen);
+        console.log("pokemen: ", pokemen);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getPokemen();
+  }, []);
+
+  useEffect(() => {
+    const getPictures = async () => {
+      try {
+        const response = await axios.get(
+          "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
+        );
+        const pokemonList = response.data.results;
+        console.log("pokemonList: ", pokemonList);
+
+        const detailedPromises = pokemonList.map((pokemon) =>
+          axios.get(pokemon.url)
+        );
+
+        const detailedResponses = await Promise.all(detailedPromises);
+        console.log("detailedresponses: ", detailedResponses);
+
+        const pokePics = detailedResponses.map((res) => ({
+          image: res.data.sprites.front_default,
+          name: res.data.name,
+        }));
+        setPokeImages(pokePics);
+        console.log("pokepics:", pokePics);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getPictures();
+  }, []);
+
+  return (
+    <div className="App">
+      <h3>PokeAPI practice</h3>
+      <img src={pokemon?.data.sprites.front_shiny} />
+      <ul>
+        {pokemen?.data?.results.map((pokemon) => (
+          <li key={pokemon.name}>
+            name: {pokemon.name}
+            {pokeImages.map((poke) =>
+              poke.name == pokemon.name ? (
+                <img key={poke.name} src={poke.image} alt={poke.name} />
+              ) : null
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
